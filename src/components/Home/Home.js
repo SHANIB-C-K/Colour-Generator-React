ead4f4035d095441aedd44a2ca026532cef0c2da
Shanib c k <ckshanib8@gmail.com>
2023-11-25 11:09:16 +0530

    color changing function is created in Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,13 +4,32 @@ import Values from "values.js";
 
 const Home = () => {
   const [list, setList] = useState(new Values("#f15025").all(10));
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try{
+        let colors = new Values(color).all(10);
+        setList(colors);
+    }catch{
+        setError(true);
+        console.log(error);
+    }
+  }
 
   return (
     <>
       <section className="container">
         <h3>Colour Generator</h3>
-        <form>
-          <input type="text" placeholder="#f15025"></input>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="#f15025"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            className={`${error ? 'error': null}`}
+          ></input>
           <button className="btn">Submit</button>
         </form>
       </section>
